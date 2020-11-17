a5593595bf4c432940ddadc28093d355f2cc8afa
jasonwlw <witryjw@gmail.com>
2020-11-16 19:11:48 -0500

    training scripts

--- Mask_RCNN-master/mrcnn/model.py
+++ Mask_RCNN-master/mrcnn/model.py
@@ -1212,6 +1212,10 @@ def load_image_gt(dataset, config, image_id, augment=False, augmentation=None,
     # Load image and mask
     image = dataset.load_image(image_id)
     mask, class_ids = dataset.load_mask(image_id)
+    print(image.shape)
+    print(mask.shape)
+    print(config.IMAGE_MIN_DIM)
+    print(config.IMAGE_MAX_DIM)
     original_shape = image.shape
     image, window, scale, padding, crop = utils.resize_image(
         image,
