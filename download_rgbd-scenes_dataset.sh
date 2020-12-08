11a84706aded818833a93673e4eb275d23c9ed7a
jasonwlw <witryjw@gmail.com>
2020-12-07 22:42:40 -0500

    Remove

--- download_rgbd-scenes_dataset.sh
+++
@@ -1,9 +0,0 @@
-wget https://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes/rgbd-scenes.tar
-tar -xvf rgbd-scenes.tar
-rm rgbd-scenes.tar
-
-wget http://rgbd-dataset.cs.washington.edu/dataset/rgbd-scenes-v2/rgbd-scenes-v2_imgs.zip
-unzip rgbd-scenes-v2_imgs.zip
-rm rgbd-scenes-v2_imgs.zip
-
-
